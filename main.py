744bd8f7aac9403ca3c8ac423e718489a4225419
bloodykatana47 <mirazizmir2005@gmail.com>
2024-03-22 20:44:13 +0500

    Tiny improvements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import re
 
 from dotenv import load_dotenv
-from pyrogram import Client, filters
+from pyrogram import Client, filters, idle
 from pyrogram.handlers import MessageHandler
 from pyrogram.types import Message
 
@@ -37,8 +37,39 @@ words_list = [word[:-1].strip() for word in words]
 print(words_list)
 
 
+async def on_startup():
+    """
+    Sending startup message
+    :return:
+    """
+    await app.send_message(chat_id='me', text=f'Бот запущен!')
+
+
+async def main():
+    """
+    Main function for starting and stopping the bot
+    :return:
+    """
+    await app.start()
+    await on_startup()
+    await idle()
+    await app.stop()
+
+
 async def forwarder(client: Client, message: Message):
+    """
+    Forwarding all messages
+    :param client:
+    :param message:
+    :return:
+    """
     await message.forward('me')
+
+
+# async def list_chats(client: Client, message: Message):
+#     async for dialog in app.get_dialogs():
+#         print(dialog.chat.id)
+#     await app.send_message(chat_id='me', text='LISTED')
 
 
 app = Client(name="my_account", api_id=API_ID, api_hash=API_HASH)
@@ -46,6 +77,12 @@ app = Client(name="my_account", api_id=API_ID, api_hash=API_HASH)
 regex_handler = MessageHandler(
     forwarder, filters=filters.chat(chats=chats_list) & filters.regex(r"(?i)\b(" + '|'.join(words_list) + r")\b")
 )
-app.add_handler(regex_handler)
 
-app.run()
+# command_list_handler = MessageHandler(
+#     list_chats, filters=filters.command(commands=['list'])
+# )
+app.add_handler(regex_handler)
+# app.add_handler(command_list_handler)
+
+if __name__ == '__main__':
+    app.run(main())
