44269c1e937970115cbdeb21aee16f0999b07cd6
bloodykatana47 <mirazizmir2005@gmail.com>
2024-04-17 11:11:01 +0500

    Tiny improvements

--- create_database.py
+++ create_database.py
@@ -8,12 +8,14 @@ def create_database() -> None:
     database = sqlite3.connect('users.db')
     cursor = database.cursor()
 
-    cursor.executescript('''
+    cursor.executescript(
+        '''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER UNIQUE NOT NULL
-        )
-    ''')
+        );
+        '''
+    )
 
     database.commit()
     database.close()
